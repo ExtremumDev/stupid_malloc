9d5455a529633a5a7d4a1055f4cf253f201f5ec0
Extremum <vlados8118@gmail.com>
2026-08-09 11:33:04 +1000

    Makefile for gui

--- test/gui/main.cpp
+++ test/gui/main.cpp
@@ -12,7 +12,7 @@ int main()
 
     for (int i = 0; i < 10; i++) {
         printf("Allcoation number %d\n", i);
-        t.malloc(10);
+        new_all = t.malloc(10);
         usleep(1500000);
     }
     return 0;
